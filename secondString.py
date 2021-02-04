20fce14138ca89105eff6185bd423b8943edf3bd
Lynch <E.Lynch@kostal.com>
2021-02-04 08:57:10 +0000

    Week03 Task

---
+++ secondString.py
@@ -0,0 +1,10 @@
+#Task week 3
+#Author: Enda Lynch
+
+#reverse a sentance and output every second letter in the string
+
+string = input('Please enter a sentance:') # input 
+
+#reversestring and output every second letter
+
+print(string [::-2])
