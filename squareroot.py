34a657aeadb3a4e0c0f6f69fec6d6af11aaad562
Lynch <E.Lynch@kostal.com>
2021-03-01 23:44:36 +0000

    Week06

--- squareroot.py
+++ squareroot.py
@@ -4,15 +4,16 @@
 #REF https://www.youtube.com/watch?v=WsQQvHm4lSw - Understand Calculas
 #REF https://www.homeschoolmath.net/teaching/square-root-algorithm.php
 #REF https://www.goeduhub.com/3398/python-program-to-find-the-square-root-number-newtons-method
+#Ref https://www.school-for-champions.com/algebra/square_root_approx.htm#.YD102-j7TDe 
+#REF Automate the Boring Stuff - Chapter 3 Functions
 
 
 a = input("Number to get square root of:" )
 
-def sqrt(number, number_iters = 1000):
+def sqrt(number, iterations = 100):
     a = float(number) # number to get square root of
-    for i in range(number_iters): # iteration number
-        number = 0.5 * (number + a / number) 
-	  # x_(n+1) = 0.5 * (x_n +a / x_n)
-    return float(number)
+    for i in range(1, iterations): # iteration number
+        number = 0.5 * (number + a / number) # √ number ≈ .5*(a/number + number)	  
+    return round((number), 1)
 
 print ("Square number of", a, "is approx:", (sqrt(float(a))))
